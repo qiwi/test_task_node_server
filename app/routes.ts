189e9817994b2b4fe13a3de72745f74513ea27aa
Evgenii <magicwolf9@mail.ru>
2018-04-21 12:19:59 +0300

    Added getAggrBills api route in routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,16 @@
 import * as config from 'config';
 import * as Router from 'koa-router';
+import { AggrBillsController } from "./controllers/aggr_bills";
 import { AuthController } from "./controllers/auth";
 import { Users as UsersController } from './controllers/users';
 
 const router = new Router();
 const users = new UsersController();
 const auth = new AuthController();
+const aggrBills = new AggrBillsController();
 
 const usersProtectedRoute = config.get('appConfig.apiPrefix') + 'users/';
+const aggrBillsProtectedRoute = config.get('appConfig.apiPrefix') + 'aggrBills';
 const authPublicRoute = config.get('appConfig.publicApiPrefix') + 'auth/';
 
 router
@@ -61,6 +64,26 @@ router
      *
      * @apiSuccess {Object} result пользователь.
      */
-    .get(usersProtectedRoute + 'item', users.getItem);
+    .get(usersProtectedRoute + 'item', users.getItem)
+    /**
+     * @api {get} /api/aggrBills
+     * @apiName getAggrBills
+     * @apiGroup AggrBills
+     *
+     * @apiDescription Возвращает записи из таблицы aggr_bills в определенном временном интервале, если какая-то
+     * из границ интервала не передана или не может быть преобразована в формат даты, то вместо неё используется
+     * граничное значение.(Для начала - 01.01.1970T00:00:00, для конца - текущая дата и время)
+     *
+     * @apiHeader (Authorization) authorization Authorization value.
+     * @apiHeaderExample Headers-Example:
+     *   { "Authorization": "Bearer :jwtToken" }
+     *
+     * @apiParam {string} (optional) Начало временного интервала для фильтровки по дате.
+     * @apiParam {string} (optional) Конец временного интервала для фильтровки по дате.
+     * Строки параметров должны конвертироваться в формат Date.
+     *
+     * @apiSuccess {Object} result записи из таблицы.
+     */
+    .get(aggrBillsProtectedRoute, aggrBills.getAggrBills);
 
 export { router };
