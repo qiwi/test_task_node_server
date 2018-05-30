ef7bf8da8e09329bfcb809a942a352e72abbba9e
Vladimir Kuzmichev <lukmann93@mail.ru>
2018-05-30 19:53:58 +0300

    Реализация метода, отдающего платёжные данные

---
+++ app/controllers/aggrBills.ts
@@ -0,0 +1,24 @@
+﻿import { Controller, ItemValidator, ValidationError } from 'innots';
+import { Context } from 'koa';
+import { AggrBillsModel } from '../models/aggrBills';
+
+const aggrBillsModel = new AggrBillsModel();
+
+export class AggrBills extends Controller {
+
+    public getItems = async (ctx: Context): Promise<void> => {
+        const fromDate: Date = this.validate(ctx, (validator: ItemValidator) => {
+            return validator.optional.isDate('fromDate');
+        });
+
+        const toDate: Date = this.validate(ctx, (validator: ItemValidator) => {
+            return validator.optional.isDate('toDate');
+        });
+
+        if (fromDate > toDate) {
+            throw new ValidationError(ValidationError.VALIDATION);
+        }
+
+        ctx.body = await aggrBillsModel.getItems(fromDate, toDate);
+    }
+}
---
+++ app/models/aggrBills.ts
@@ -0,0 +1,30 @@
+﻿import { pgService } from "../../app";
+
+export interface IAggrBill {
+	idBills: number;
+	count: number;
+	amount: number;
+	paidCount: number;
+	paidAmount: number;
+	addTimeStamp: Date;
+}
+
+export class AggrBillsModel {
+    public async getItems(fromDate: Date | null, toDate: Date | null): Promise<Array<IAggrBill>> {
+        let filter = "";
+
+        if (fromDate) {
+            filter += `WHERE bills_add_timestamp >= '${fromDate.toISOString()}' `;
+        }
+
+        if (toDate) {
+            filter += filter ? `AND bills_add_timestamp < '${toDate.toISOString()}' ` : `WHERE bills_add_timestamp < '${toDate.toISOString()}' `;
+        }
+
+        return await pgService.getRows(`
+            SELECT id_bills, bills_count, bills_amount, bills_paid_count, bills_paid_amount, bills_add_timestamp
+            FROM aggr_bills
+            ${filter ? filter : ""}
+        `);
+    }
+}
