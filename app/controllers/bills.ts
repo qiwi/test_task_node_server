f80cb3fd11a8b18f567ad1628e7c3404de7d492d
tema <regibox@icloud.com>
2019-01-17 13:21:02 +0300

    added todos in comment

--- app/controllers/bills.ts
+++ app/controllers/bills.ts
@@ -10,15 +10,23 @@ export class Bills extends Controller {
     //     ctx.body = await billsModel.getItems();
     // }
 
+    // TODO:
+    // 1) тип данных не string, а Date
+    // 2) в новом innots есть isDate!
+    // 3) сравение from < to c выдачей ошибки, если это не так
+    // 4) limit и offset (возможно POST)
     public getItems = async (ctx: Context): Promise<void> => {
-        const billsDateFrom: string = this.validate(ctx, (validator: ItemValidator) => {
+        const DateFromStr: string = this.validate(ctx, (validator: ItemValidator) => {
             return validator.optional.isString('from');
         });
-        const billsDateTo: string = this.validate(ctx, (validator: ItemValidator) => {
+        const DateToStr: string = this.validate(ctx, (validator: ItemValidator) => {
             return validator.optional.isString('to');
         });
 
-        ctx.body = await billsModel.getItems(billsDateFrom, billsDateTo);
+        // const DateFrom = new Date(DateFromStr);
+        // const DateTo = new Date(DateToStr);
+
+        ctx.body = await billsModel.getItems(DateFromStr, DateToStr);
 
     }
 
