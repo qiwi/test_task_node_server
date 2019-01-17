e3a1d7d071637fdfee790254c65afe754b7b8eb7
tema <regibox@icloud.com>
2019-01-17 22:28:24 +0300

    Generate Docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,16 +1,1 @@
-define({
-  "name": "Test task node server",
-  "version": "1.0.0",
-  "description": "Fullstack JS / TS developer test task basic app",
-  "title": "Test task node server",
-  "url": "",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-01-17T17:12:14.189Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "Test task node server",  "version": "1.0.0",  "description": "Fullstack JS / TS developer test task basic app",  "title": "Test task node server",  "url": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-01-17T19:28:02.672Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
