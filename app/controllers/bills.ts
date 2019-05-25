9c6e39110062f9a726939a0ed2bc3136868b19fb
BANO.notIT <bano.notit@yandex.ru>
2019-05-25 23:08:39 +0300

    core(controller): Add bills controller with time query

---
+++ app/controllers/bills.ts
@@ -0,0 +1,19 @@
+import {Controller, InnoError} from 'innots';
+import {Context} from 'koa';
+import {BillsModel} from "../models/bills";
+
+const billsModel = new BillsModel();
+
+export class BillsController extends Controller {
+
+    public getItems = async (ctx: Context): Promise<void> => {
+        const {from, to} = ctx.validatedData.camelCase;
+
+        if (from && to && from > to) {
+            throw new InnoError('INVALID_REQUEST_PARAMS', 400, "from must be less then to");
+        }
+
+        ctx.body = billsModel.getItems(ctx.validatedData.camelCase);
+    }
+
+}
